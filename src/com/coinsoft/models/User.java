4d906605b56ac6bee4a6133af457c6deb6a01c45
J0hns <1410352@utp.edu.pe>
2017-11-10 02:02:20 -0500

    Agregando User

--- src/com/coinsoft/models/User.java
+++ src/com/coinsoft/models/User.java
@@ -6,8 +6,74 @@ import java.sql.SQLException;
 public class User {
     private int id;
     private String user;
-    private String passwordd;
+    private String password;
     private int type;
     private String status;
     private Person person;
+
+    public User() {
+    }
+
+    public User(int id, String user, String password, int type, String status, Person person) {
+        this.id = id;
+        this.user = user;
+        this.password = password;
+        this.type = type;
+        this.status = status;
+        this.person = person;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public User setUser(String user) {
+        this.user = user;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public User setType(int type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public User setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public User setPerson(Person person) {
+        this.person = person;
+        return this;
+    }
 }
