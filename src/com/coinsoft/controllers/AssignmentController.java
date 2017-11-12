d58fe06d6403f1ac779ff4e3030ff2bec25d6930
Said Vega Mendoza <1410792@utp.edu.pe>
2017-11-11 19:25:01 -0500

    controllers address assignment audittrail

--- src/com/coinsoft/controllers/AssignmentController.java
+++ src/com/coinsoft/controllers/AssignmentController.java
@@ -1,4 +1,86 @@
 package com.coinsoft.controllers;
+import com.coinsoft.models.CmService;
+import com.coinsoft.models.Assignment;
 
-public class AssignmentController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@javax.servlet.annotation.WebServlet(name = "AssignmentController", urlPatterns = "/assignment")
+public class AssignmentController  extends javax.servlet.http.HttpServlet {
+
+
+    CmService service;
+    String url;
+
+    public AssignmentController() {
+        super();
+        service = new CmService();
+        url = "";
+    }
+
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        processRequest("POST", request, response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        processRequest("GET", request, response);
+    }
+
+    private void processRequest(String method, HttpServletRequest request, HttpServletResponse response)  throws javax.servlet.ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if(method.equals("GET")) {
+            // Index Action
+            if(action.equals("index")) {
+               // List<Assignment> assignments = service.findAllAssignment();
+                //request.setAttribute("assignment", addresses);
+                url = "listAssignment.jsp";
+            }
+            if(action.equals("show")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+                //Assignment assignment = service.findAssignmentById(id);
+                //request.setAttribute("assignment",assignment);
+                url = "showAssignment.jsp";
+            }
+            if(action.equals("new")) {
+                url = "newAssignment.jsp";
+            }
+            if(action.equals("edit")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+               // Assignment assignment = service.findAssignmentById(id);
+               // request.setAttribute("assignment", assignment);
+                url = "editAssignment.jsp";
+            }
+        }
+
+        if(method.equals("POST")) {
+            // Create Action
+            if(action.equals("create")) {
+                String name = request.getParameter("name");
+              //  Assignment address = service.createAssignment(name);
+              //  List<Assignment> assignments = service.findAllAssignment();
+              //  request.setAttribute("assignment", assignments);
+                url = "listAssignment.jsp";
+            }
+            if(action.equals("update")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+                String name = request.getParameter("name");
+             //   boolean isUpdated = service.updateAssignment(id, name);
+             //   List<Assignment> assignments = service.findAllAssignment();
+             //   request.setAttribute("assignments", assignments);
+                url = "listAssignment.jsp";
+            }
+        }
+
+        request.getRequestDispatcher(url).forward(request, response);
+    }
+
+
+
+
+
+
 }
--- src/com/coinsoft/controllers/AuditTrailController.java
+++ src/com/coinsoft/controllers/AuditTrailController.java
@@ -1,4 +1,85 @@
 package com.coinsoft.controllers;
 
-public class AuditTrailController {
+import com.coinsoft.models.CmService;
+import com.coinsoft.models.AuditTrail;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+
+@javax.servlet.annotation.WebServlet(name = "AssignmentController", urlPatterns = "/assignment")
+public class AuditTrailController  extends javax.servlet.http.HttpServlet {
+
+
+    CmService service;
+    String url;
+
+    public AuditTrailController() {
+        super();
+        service = new CmService();
+        url = "";
+    }
+
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        processRequest("POST", request, response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        processRequest("GET", request, response);
+    }
+
+    private void processRequest(String method, HttpServletRequest request, HttpServletResponse response)  throws javax.servlet.ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if(method.equals("GET")) {
+            // Index Action
+            if(action.equals("index")) {
+              //  List<AuditTrail> auditTrails = service.findAllAuditTrail();
+              //  request.setAttribute(" auditTrail", auditTrails);
+                url = "listAuditTrail.jsp";
+            }
+            if(action.equals("show")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+             //   AuditTrail auditTrail = service.findAuditTrailById(id);
+             //   request.setAttribute("auditTrail",auditTrail);
+                url = "showAuditTrail.jsp";
+            }
+            if(action.equals("new")) {
+                url = "newAuditTrail.jsp";
+            }
+            if(action.equals("edit")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+            //    AuditTrail auditTrail = service.findAuditTrailById(id);
+              //  request.setAttribute("auditTrail", auditTrail);
+                url = "editAuditTrail.jsp";
+            }
+        }
+
+        if(method.equals("POST")) {
+            // Create Action
+            if(action.equals("create")) {
+                String name = request.getParameter("name");
+              //  AuditTrail auditTrail = service.createAuditTrail(name);
+              //  List<AuditTrail> assignments = service.findAllAuditTrail();
+               // request.setAttribute("auditTrail", auditTrail);
+                url = "listAuditTrail.jsp";
+            }
+            if(action.equals("update")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+                String name = request.getParameter("name");
+             //   boolean isUpdated = service.updateAuditTrail(id, name);
+             //   List<AuditTrail> auditTrails = service.findAllAuditTrail();
+             //   request.setAttribute("auditTrail", auditTrails);
+                url = "listAuditTrail.jsp";
+            }
+        }
+
+        request.getRequestDispatcher(url).forward(request, response);
+    }
+
+
 }
