42a8a129e7dfa319ed5af0627e005d5efc07474b
KEVIN COTERA BELLO <1330553@utp.edu.pe>
2017-11-11 19:56:21 -0500

    .

--- src/com/coinsoft/models/CmDataStore.java
+++ src/com/coinsoft/models/CmDataStore.java
@@ -34,9 +34,86 @@ public class CmDataStore {
         return getEmployeesEntity().findById(id);
     }
 
-    public Loan findLoanById(String id) {
+    /*public Loan findLoanById(int id) {
         if(connection == null) return null;
         return getLoansEntity().findById(id);
     }
 
+    public Visit findVisitById(int id) {
+        if(connection == null) return null;
+        return getVisitsEntity().findById(id);
+    }*/
+
+    public List<Assignment> findAllAssignment() {
+
+        return connection == null ? null: getAssignmentsEntity().findAll();
+    }
+
+    public List<Customer> findAllCustomer() {
+
+        return connection == null ? null : getCustomersEntity().findAll();
+    }
+
+    public List<Employe> findAllEmploye() {
+
+        return connection == null ? null: getEmployeesEntity().findAll();
+    }
+
+    /*public List<Loan> findAllLoan() {
+
+        return connection == null ? null : getLoansEntity().findAll();
+    }
+
+    public List<Visit> findAllVisit() {
+
+        return connection == null ? null : getVisitsEntity().findAll();
+    }*/
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    private AssignmentsEntity getAssignmentsEntity() {
+        if(assignmentsEntity == null) {
+            assignmentsEntity = new AssignmentsEntity();
+            assignmentsEntity.setConnection(connection);
+        }
+        return assignmentsEntity;
+    }
+
+    private CustomersEntity getCustomersEntity() {
+        if(customersEntity == null) {
+            customersEntity = new CustomersEntity();
+            customersEntity.setConnection(connection);
+        }
+        return customersEntity;
+    }
+
+    private EmployeesEntity getEmployeesEntity() {
+        if(employeesEntity == null) {
+            employeesEntity = new EmployeesEntity();
+            employeesEntity.setConnection(connection);
+        }
+        return employeesEntity;
+    }
+
+    private LoansEntity getLoansEntity() {
+        if(loansEntity == null) {
+            loansEntity = new LoansEntity();
+            loansEntity.setConnection(connection);
+        }
+        return loansEntity;
+    }
+
+    /*private VisitsEntity getVisitsEntity() {
+        if(visitsEntity == null) {
+            visitsEntity = new VisitsEntity();
+            visitsEntity.setConnection(connection);
+        }
+        return visitsEntity;
+    }*/
 }
