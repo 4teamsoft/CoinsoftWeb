6d72d4ac9d04d1a8bf3571f585144054828d9f7d
J0hns <1410352@utp.edu.pe>
2017-11-11 21:31:38 -0500

    upload

--- src/com/coinsoft/controllers/UserController.java
+++ src/com/coinsoft/controllers/UserController.java
@@ -1,4 +1,78 @@
 package com.coinsoft.controllers;
 
-public class UserController {
+import com.coinsoft.models.CmService;
+import com.coinsoft.models.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@javax.servlet.annotation.WebServlet(name = "RegionsController", urlPatterns = "/user")
+public class UsersController extends javax.servlet.http.HttpServlet {
+    CmService service;
+    String url;
+
+    public UserController() {
+        super();
+        service = new CmService();
+        url = "";
+    }
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        processRequest("POST", request, response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        processRequest("GET", request, response);
+    }
+
+    private void processRequest(String method, HttpServletRequest request, HttpServletResponse response)  throws javax.servlet.ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if(method.equals("GET")) {
+            // Index Action
+            if(action.equals("index")) {
+                List<User> users = service.findAllRegions();
+                request.setAttribute("users", users);
+                url = "listUser.jsp";
+            }
+            if(action.equals("show")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+                User region = service.findRegionById(id);
+                request.setAttribute("user", user);
+                url = "showUser.jsp";
+            }
+            if(action.equals("new")) {
+                url = "newUser.jsp";
+            }
+            if(action.equals("edit")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+                User user = service.findRegionById(id);
+                request.setAttribute("user", user);
+                url = "editUser.jsp";
+            }
+        }
+
+        if(method.equals("POST")) {
+            // Create Action
+            if(action.equals("create")) {
+                String name = request.getParameter("name");
+                Region region = service.createRegion(name);
+                List<Region> regions = service.findAllRegions();
+                request.setAttribute("regions", regions);
+                url = "listRegions.jsp";
+            }
+            if(action.equals("update")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+                String name = request.getParameter("name");
+                boolean isUpdated = service.updateRegion(id, name);
+                List<Region> regions = service.findAllRegions();
+                request.setAttribute("regions", regions);
+                url = "listRegions.jsp";
+            }
+        }
+
+        request.getRequestDispatcher(url).forward(request, response);
+    }
 }
