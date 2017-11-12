332e62afacd171966024dbaf2c418d95ca610fd8
Felix Santiago Vargas Reyes <1330187@utp.edu.pe>
2017-11-11 19:36:45 -0500

    Actualizacion clases

--- src/com/coinsoft/models/CmService.java
+++ src/com/coinsoft/models/CmService.java
@@ -9,6 +9,32 @@ import java.util.List;
 
 public class CmService {
 
+    private Connection connection;
+    private CmDataStore dataStore;
+
+    public CmService() {
+        try {
+            InitialContext context = new InitialContext();
+            dataStore = new CmDataStore();
+            connection = ((DataSource) context
+                    .lookup("jdbc/MySQLDataSource"))
+                    .getConnection();
+            dataStore.setConnection(connection);
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() { return connection; }
+
+    public void setConnection(Connection connection) {this.connection = connection; }
+
+    public CmDataStore getDataStore() { return dataStore; }
+
+    public void setDataStore(CmDataStore dataStore) {  this.dataStore = dataStore;  }
+
 
 
 }
