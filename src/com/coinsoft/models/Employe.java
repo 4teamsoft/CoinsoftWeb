5071fa62a5754a37450c52b63a1bcf65f0a66d20
KEVIN COTERA BELLO <1330553@utp.edu.pe>
2017-11-13 01:27:39 -0500

    .

--- src/com/coinsoft/models/Employe.java
+++ src/com/coinsoft/models/Employe.java
@@ -7,17 +7,22 @@ public class Employe extends Person {
     private Employe manager;
     private Employe admin;
 
-
-
-    public Employe(int id, String code, String dni, String name, String lastName, int age, String mail, String status) {
-        super(id, code, dni, name, lastName, age, mail, status);
-
+    public Employe() {
     }
 
-    public Employe(int id, String code, String dni, String name, String lastName, int age, String mail, String status, Employe manager, Employe admin) {
-        super(id, code, dni, name, lastName, age, mail, status);
-        this.setManager(manager);
-        this.setAdmin(admin);
+    public Employe(String code, String dni, String name, String last_name, int age, String mail, String status, Employe manager, Employe admin) {
+        this.manager = manager;
+        this.admin = admin;
+    }
+
+    public Employe(String code, String dni, String name, String lastName, int age, String mail) {
+        super(code, dni, name, lastName, age, mail);
+    }
+
+    public Employe(String code, String dni, String name, String lastName, int age, String mail, Employe manager, Employe admin) {
+        super(code, dni, name, lastName, age, mail);
+        this.manager = manager;
+        this.admin = admin;
     }
 
     public Employe getManager() {
@@ -42,7 +47,6 @@ public class Employe extends Person {
     public static Employe from(ResultSet rs, EmployeesEntity employeesEntity) {
         try {
             return new Employe(
-                    rs.getInt("id"),
                     rs.getString("code"),
                     rs.getString("dni"),
                     rs.getString("name"),
