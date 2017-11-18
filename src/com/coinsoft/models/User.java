023f2409fa03ffd739cbfed92f14b5cf074d9696
J0hns <1410352@utp.edu.pe>
2017-11-18 17:37:16 -0500

    parch

--- src/com/coinsoft/models/User.java
+++ src/com/coinsoft/models/User.java
@@ -1,14 +1,14 @@
 package com.coinsoft.models;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class User extends Employe{
 
-    private int id;
     private String user;
     private String password;
-    private int type;
+    private String type;
     private String status;
     private int employeId;
 
@@ -16,17 +16,34 @@ public class User extends Employe{
     public User() {
     }
 
+    public User(String code, String dni, String name, String lastName, int age, String mail, Date startDate, Date endDate, String employeStatus, String user, String password, String type, String status, int employeId) {
+        super(code, dni, name, lastName, age, mail, startDate, endDate, employeStatus);
+
+        this.user = user;
+        this.password = password;
+        this.type = type;
+        this.status = status;
+        this.employeId = employeId;
+    }
+
     public static User from(ResultSet rs) {
         try {
-            return new Customer(
+            return new User(
                     rs.getString("code"),
                     rs.getString("dni"),
                     rs.getString("name"),
                     rs.getString("last_name"),
                     rs.getInt("age"),
                     rs.getString("mail"),
-                    rs.getInt("id"),
-                    rs.getString("status")
+                    rs.getDate("startDate"),
+                    rs.getDate("endDate"),
+                    rs.getString("employeStatus"),
+                    rs.getString("user"),
+                    rs.getString("password"),
+                    rs.getString("type"),
+                    rs.getString("status"),
+                    rs.getInt("employeId")
+
             );
         } catch (SQLException e) {
             e.printStackTrace();
