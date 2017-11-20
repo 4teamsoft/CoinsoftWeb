ec48d7b201ff5b873831291f0cddb16608196fe0
Felix Santiago Vargas Reyes <1330187@utp.edu.pe>
2017-11-20 02:00:08 -0500

    .

--- src/com/coinsoft/models/Customer1.java
+++ src/com/coinsoft/models/Customer1.java
@@ -35,17 +35,23 @@ public class Customer1 extends Person1 {
     public static Customer1 from(ResultSet rs) {
         try {
             return new Customer1(
-                    rs.getInt(String.valueOf(Person1.from(rs).setPersonId(rs.getInt("person_id")))),
-                    //rs.getInt(Person1.from(rs.getInt("aa"))),
-                    //rs.getInt(Person1.from(rs).setPersonId(rs.getInt("")) ),
                     //rs.getInt("person_id"),
-                    rs.getString("code"),
-                    rs.getString("dni"),
-                    rs.getString("name"),
-                    rs.getString("last_name"),
-                    rs.getInt("age"),
-                    rs.getString("mail"),
-                    rs.getString("person_status"),
+                    rs.getInt(String.valueOf(Person1.from(rs).setPersonId(rs.getInt("person_id")))),
+                    //rs.getString("code"),
+                    rs.getString(String.valueOf(Person1.from(rs).setCode(rs.getString("code")))),
+                    //rs.getString("dni"),
+                    rs.getString(String.valueOf(Person1.from(rs).setDni(rs.getString("dni")))),
+                    //rs.getString("name"),
+                    rs.getString(String.valueOf(Person1.from(rs).setDni(rs.getString("name")))),
+                    // rs.getString("last_name"),
+                    rs.getString(String.valueOf(Person1.from(rs).setDni(rs.getString("last_name")))),
+                    //rs.getInt("age"),
+                    rs.getInt(String.valueOf(Person1.from(rs).setPersonId(rs.getInt("age")))),
+                    //rs.getString("mail"),
+                    rs.getString(String.valueOf(Person1.from(rs).setDni(rs.getString("mail")))),
+                   // rs.getString("person_status"),
+                    rs.getString(String.valueOf(Person1.from(rs).setDni(rs.getString("person_status")))),
+
                     rs.getString("status")
             );
         } catch (SQLException e) {
