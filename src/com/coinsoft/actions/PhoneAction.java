b9e20f2a8ea07e15de23da7e65b6c7225ef03add
Said Vega Mendoza <1410792@utp.edu.pe>
2017-11-24 19:43:12 -0500

    action validations y action phones

--- src/com/coinsoft/actions/PhoneAction.java
+++ src/com/coinsoft/actions/PhoneAction.java
@@ -1,12 +1,44 @@
 package com.coinsoft.actions;
 
-public class PhoneAction {
+import com.opensymphony.xwork2.ActionSupport;
 
+public class PhoneAction extends ActionSupport{
 
+    private String number;
+    private String type;
+    private String operator;
 
+    public PhoneAction(String number, String type, String operator) {
+        this.number = number;
+        this.type = type;
+        this.operator = operator;
+    }
 
+    public String getNumber() {
+        return number;
+    }
 
+    public void setNumber(String number) {
+        this.number = number;
+    }
 
+    public String getType() {
+        return type;
+    }
 
+    public void setType(String type) {
+        this.type = type;
+    }
 
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String execute() {
+        return SUCCESS;
+    }
 }
